33c5263b0a6c5fd21a3c29d9c2c3649d78b2fdf2
Abhinav <abhinav.maurya202@gmail.com>
2022-06-23 23:30:10 +0530

    update funtaniality added in backend!!

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -32,7 +32,23 @@ exports.create = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
+        const phone = await Phone({ _id: req.query.id });
 
+        if (!phone) {
+            return res.status(500).json({
+                message: error.message
+            });
+        }
+        const newPhone = await Phone.findByIdAndUpdate(req.query.id, req.body, {
+            new: true,
+            runValidator: true,
+            useFindAndModify: false
+        })
+
+        res.status(200).json({
+            sucesss: true,
+            newPhone
+        })
     } catch (error) {
         res.status(500).json({
             message: error.message
@@ -53,8 +69,8 @@ exports.deleteContact = async (req, res) => {
         await Phone.findByIdAndDelete(req.query.id);
 
         res.status(200).json({
-            success:true,
-            message:"deleted sucessully"
+            success: true,
+            message: "deleted sucessully"
         })
 
     } catch (error) {
